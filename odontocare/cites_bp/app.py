f362a692f8398868826a35e05896c562b3a545c7
Oriol Cantarell i Sala <ocantarelli@uoc.edu>
2025-12-28 18:43:44 +0100

    define cites blueprint

--- odontocare/cites_bp/app.py
+++ odontocare/cites_bp/app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from odontocare import cites_bp
 
 def create_app():
     app = Flask(__name__)
+
+    # Registramos el Blueprint de cites
+    app.register_blueprint(cites_bp, url_prefix='/api/v1')
     
     return app
---
+++ odontocare/cites_bp/recursos/cites.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, jsonify, request
+
+# Creamos una instancia de Blueprint
+# 'cites_bp' es el nombre del Blueprint
+# El segundo parámetro es el nombre del módulo
+cites_bp = Blueprint('cites_bp', __name__)
+
+# Definimos las rutas usando el Blueprint
+@cites_bp.route('/cites', methods=['POST'])
+def add_appointment():
+    pass
+
+@cites_bp.route('/cites', methods=['GET'])
+def get_appointment():
+    pass
+
+@cites_bp.route('/cites/<int:id>', methods=['PUT'])
+def cancel_appointment(id):
+    pass
+
+# ... (Añadir aquí las rutas POST, PUT, DELETE para citas)
