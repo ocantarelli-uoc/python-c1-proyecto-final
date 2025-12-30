088d27004249d99d61394d4236c60a7c1dd7bb7a
Oriol Cantarell i Sala <ocantarelli@uoc.edu>
2025-12-30 16:39:48 +0100

    Create create_center.py

---
+++ odontocare/auth_and_admin_bp/admin_bp/centers/services/create_center.py
@@ -0,0 +1,13 @@
+from flask import request
+from models.MedicalCenter import MedicalCenter
+from models.Address import Address
+from extensions import db
+
+def create_center():
+    datos = request.get_json()
+    address:Address = Address.query.filter_by(name=datos["address_name"]).first()
+    medical_center = MedicalCenter(name=datos["name"],
+                                   address=address)
+    db.session.add(medical_center)
+    db.session.commit()
+    return medical_center
