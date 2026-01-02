40e72e0bdeedf9743e5e2109d055b0becd17a1c5
Oriol Cantarell i Sala <ocantarelli@uoc.edu>
2026-01-02 15:01:32 +0100

    Update get_user_role_by_name.py

--- odontocare/auth_and_admin_bp/admin_bp/user_roles/services/get_user_role_by_name.py
+++ odontocare/auth_and_admin_bp/admin_bp/user_roles/services/get_user_role_by_name.py
@@ -1,5 +1,5 @@
 from models.UserRole import UserRole
 
 def get_user_role_by_name(role_name):
-    user_role = UserRole.query.filter_by(role_name=role_name).first()
+    user_role = UserRole.query.filter_by(name=role_name).first()
     return user_role
