e0bfed11fcf884f220ab55e49e9a3b72b3067bab
Oriol Cantarell i Sala <ocantarelli@uoc.edu>
2026-01-11 00:04:07 +0100

    Modify first admin app user password for config values .env

--- odontocare/auth_and_admin_bp/first_configs.py
+++ odontocare/auth_and_admin_bp/first_configs.py
@@ -1,10 +1,12 @@
 from admin_bp.user_roles.services.create_user_role import create_user_role
 from admin_bp.users.services.create_user import create_user
+from dotenv import dotenv_values
 
 def create_first_admin_user():
+    config_dotenv_values = dotenv_values(".env")
     created_role = create_user_role({'role_name':'admin'})
     created_user = create_user({
-        'username':'admin',
-        'password':'test2xa'
+        'username':config_dotenv_values['ADMIN_FIRST_APP_USER_USERNAME'],
+        'password':config_dotenv_values['ADMIN_FIRST_APP_USER_PASSWORD']
     },'admin')
     return created_user
