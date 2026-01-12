bd87baccc8dba8fe012110f3425f56da24a5872a
Oriol Cantarell i Sala <ocantarelli@uoc.edu>
2026-01-12 21:05:29 +0100

    service for login from charger

---
+++ cargador_inicial/dtos/Token.py
@@ -0,0 +1,3 @@
+class Token: 
+    def __init__(self,token:str):
+        self.token = token
---
+++ cargador_inicial/services/LoginService.py
@@ -0,0 +1,19 @@
+import sys
+from flask import request
+import requests
+from dtos.Token import Token
+
+def login(username:str,password:str) -> Token:
+    body_payload:dict={
+        "user":username,
+        "password":password
+    }
+    req = requests.post('http://localhost:5001/api/v1/auth/login',json=body_payload)
+    r = req.prepare()
+    r.headers['Authorization'] = request.headers.get('Authorization')
+    r.headers['Content-Type'] = 'application/json'   
+    s = requests.Session()
+    rs: requests.Response = s.send(r)
+    token_list = rs.json()
+    token : Token = Token(token=token_list[0]['token'])
+    return token
