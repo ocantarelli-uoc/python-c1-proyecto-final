0fa1fbad8a6313d5bca8d38857bc866d4ff0d9c3
Oriol Cantarell i Sala <ocantarelli@uoc.edu>
2026-01-18 14:18:22 +0100

    Comments on carga_inicial.py

--- odontocare/cargador_inicial/carga_inicial.py
+++ odontocare/cargador_inicial/carga_inicial.py
@@ -46,63 +46,81 @@ class CargaInicial:
         app_first_user_password : str = config_dotenv_values['ADMIN_FIRST_APP_USER_PASSWORD']
         token : Token = login(app_first_user_username,app_first_user_password)
         self.token = token
-        """It instances all the CSV File Managers intances for files and after instance it every one,
+        """It instances all the CSV File Manager intance for file dades.csv and after instance it,
         it read its content as a DataFrame with Pandas library
         """
         dadesCsvFileManager = CSVFileManager("data/dades.csv")
         dadesDataFrame = dadesCsvFileManager.read()
+        #It instances the address converter
         addressConverter = AddressConverter()
+        #It converts the rows from dades.csv data frame which are addresses
         addresses = addressConverter.convert(dadesDataFrame)
         #addressConverter.print(addresses)
+        #It instances the medical center converter
         medicalCenterConverter = MedicalCenterConverter()
+        #It converts the rows from dades.csv data frame which are medical centers
         medical_centers = medicalCenterConverter.convert(dadesDataFrame)
         #medicalCenterConverter.print(medical_centers)
+        #It instances the medical speciality converter
         medicalSpecialityConverter = MedicalSpecialityConverter()
+        #It converts the rows from dades.csv data frame which are medical specialities
         medical_specialities = medicalSpecialityConverter.convert(dadesDataFrame)
         #medicalSpecialityConverter.print(medical_specialities)
+        #It instances the user role converter
         userRolesConverter = UserRoleConverter()
+        #It converts the rows from dades.csv data frame which are user roles
         user_roles = userRolesConverter.convert(dadesDataFrame)
         #userRolesConverter.print(user_roles)
+        #It instances the user converter
         userConverter = UserConverter()
+        #It converts the rows from dades.csv data frame which are users
         users = userConverter.convert(dadesDataFrame)
         #userConverter.print(users)
+        #It instances the patient converter
         patientConverter = PatientConverter()
+        #It converts the rows from dades.csv data frame which are patients
         patients = patientConverter.convert(dadesDataFrame)
         #patientConverter.print(patients)
+        #It instances the doctor converter
         doctorConverter = DoctorConverter()
+        #It converts the rows from dades.csv data frame which are doctors
         doctors = doctorConverter.convert(dadesDataFrame)
         #doctorConverter.print(doctors)
+        #It instances the medical appointment status converter
         medicalAppointmentStatusConverter = MedicalAppointmentStatusConverter()
+        #It converts the rows from dades.csv data frame which are medical appointment statuses
         medicalAppointmentStatuses = medicalAppointmentStatusConverter.convert(dadesDataFrame)
         #medicalAppointmentStatusConverter.print(medicalAppointmentStatuses)
+        #It instances the medical appointment converter
         medicalAppointmentConverter = MedicalAppointmentConverter()
+        #It converts the rows from dades.csv data frame which are medical appointments
         medicalAppointments = medicalAppointmentConverter.convert(dadesDataFrame)
         #medicalAppointmentConverter.print(medicalAppointments)
-
+        #It creates every address
         for address in addresses:
             create_address(address=address,token=token)
-        
+        #It creates every medical center
         for medical_center in medical_centers:
             create_medical_center(medical_center=medical_center,token=token)
-        
+        #It creates every medical speciality
         for medical_speciality in medical_specialities:
             create_medical_speciality(medical_speciality=medical_speciality,token=token)
-        
+        #It creates every user role
         for user_role in user_roles:
             create_user_role(user_role=user_role,token=token)
-        
+        #It creates every user
         for user in users:
             create_user(user=user,token=token)
-
+        #It creates every patient
         for patient in patients:
             create_patient(patient=patient,token=token)
-        
+        #It creates every doctor
         for doctor in doctors:
             create_doctor(doctor=doctor,token=token)
-        
+        #It creates every medical appointment status
         for medical_appointment_status in medicalAppointmentStatuses:
             create_medical_appointment_status(medical_appointment_status=medical_appointment_status,token=token)
-        
+        #It creates every medical appointment
         for medical_appointment in medicalAppointments:
             created_medical_appointment : MedicalAppointment = create_medical_appointment(medical_appointment=medical_appointment,token=token)
             #print(created_medical_appointment.describe())
