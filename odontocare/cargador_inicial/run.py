2c283a60e6cb8107e2c2198894629639314797c4
Oriol Cantarell i Sala <ocantarelli@uoc.edu>
2026-01-18 17:13:48 +0100

    Update run.py

--- odontocare/cargador_inicial/run.py
+++ odontocare/cargador_inicial/run.py
@@ -2,5 +2,5 @@ from carga_inicial import CargaInicial
 
 
 if __name__ == '__main__':
-    cargaInicial : CargaInicial = CargaInicial(token=None)
+    cargaInicial : CargaInicial = CargaInicial()
     cargaInicial.doCharge()
